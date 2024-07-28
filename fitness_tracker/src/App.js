da132bed11dcdea264f2a0d2a49084cc5cdcee7b
Chris Valenzuela <themegas100@gmail.com>
2024-07-28 18:03:19 +1200

    feat: Added temporary display of workouts (really should be named exercises)

--- fitness_tracker/src/App.js
+++ fitness_tracker/src/App.js
@@ -1,14 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import TempWorkoutDisplay from './components/TempWorkoutDisplay';
+
 
 function App() {
+// State for storing workouts
+  const [workouts, setWorkouts] = useState([]);
+
+  const fetchWorkouts = async () => {
+    // Send GET request to 'workouts/all' endpoint
+    const response = await fetch('http://localhost:4001/workouts/all')
+
+      // Parse JSON of response
+      const workouts = await response.json()
+
+      // Set state with workouts
+      setWorkouts(workouts)
+
+      // Log workouts to console
+      console.log(workouts)
+  }
+
+  // Fetch workouts when component mounts
+  useEffect(() => {
+    fetchWorkouts()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <TempWorkoutDisplay data={workouts}/>
         <a
           className="App-link"
           href="https://reactjs.org"
